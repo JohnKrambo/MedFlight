bcd05f766cfa90c461d06f6bf4ef1b69eac02ebe
Can Kattwinkel <ck115@hdm-stuttgart.degit config --global user.email ck115@hdm-stuttgart.degit config --global user.email ck115@hdm-stuttgart.de>
2014-11-11 15:01:26 +0100

    localstorage

--- js/validate.js
+++ js/validate.js
@@ -1,4 +1,4 @@
-		/*    Validate.js checks the Logindata and handles the Customerdata loaded with verify.php
+/*    Validate.js checks the Logindata and handles the Customerdata loaded with verify.php
 			*	 
 			* Authors: Can Kattwinkel & Andreas Krappitz
 		*/
@@ -8,27 +8,35 @@
 			var password = document.getElementById('password').value;
 			
 			//createArrays() gets a JSON objekt from the Database and splits them into two Arrays keys[] &values[]
-			$.getJSON( "http://gg-dev.de/verify.php?username="+username+"&Password="+password, function createArrays( data ) {
-			  var keys = [];
-			  var values = [];
-				for (var property in data) {
-   				if ( ! data.hasOwnProperty(property)) {
-      				continue;
-   					}
-  				 keys.push(property);
-   				 values.push(data[property]);
-					}
-					if (values[0] == "true" ) {
-					//emptys the main div
-					$('#main').html("");
-					//posts the message and the emergency Button in the main div
-					$('#main').html("Hallo" + " " + values[2] + " " + values[3] + 
-					"</br>" + "Bitte nennen Sie die Art des medizinischen Notfalls:" + "</br>" + "<input type='button'name='button'value='" + values[6]+"'>");
-					}
-					else {
-					$('#main').html("Login Failed");
+				$.getJSON( "http://gg-dev.de/verify.php?username="+username+"&password="+password, function( data ) {
+				
+				var items = [];
+			  	$.each( data, function( key, val ) {
+				// legt jeweils nur den Wert in die Variable
+					items.push( val );
+			  	});
+				
+				// Werte werden nun im localstorage gespeichert wenn items[0] (LOGIN) = true
+				if (items[0] == "true" ) {
+					localStorage.setItem("login", true);
+					localStorage.setItem("id", items[1]);
+					localStorage.setItem("forename", items[2]);
+					localStorage.setItem("surname", items[3]);
+					localStorage.setItem("street", items[4]);
+					localStorage.setItem("zip", items[5]);
+					localStorage.setItem("loginDate",  new Date());
+					localStorage.setItem("disease0", items[6]);
+					localStorage.setItem("disease1", items[7]);
+					localStorage.setItem("disease2", items[8]);
+					localStorage.setItem("disease3", items[9]);
+					localStorage.setItem("disease4", items[10]);
+					alert("Daten erfolgreich im LocalStorage abgelegt")
+					window.location = "main.html";
+
+				}
+				else {
+					alert("fail")
 					}
 				});
 
 			}
-		
---
+++ old/Readme.txt
@@ -0,0 +1,15 @@
+Hier kommt einmal die Doku zu unser genialen APP rein ;)
+
+#Struktur#######
+Die App sollte sich nur auf eine Seite beschränken auf der möglichst wenige Knöpfe vorhanden sind damit
+der Nutzer sich im Notfall nicht verklicken kann.
+########
+
+#Javascript Libary#####
+Als Javascript Libary wird zepto benutzt. Hierbei handelt es sich um eine Phonegap kompatible 
+Lib die von der Nutzung sehr stark an Jquery erinnert und damit ausserdem Kompatibel ist. Der Vorteil ist, dass
+die Libary nur eine Kleine .js datei groß ist und über alle benötigten funktionen verfügt.
+
+Zur Installation der App muss der QR code mit einem Androidphone gescannt werden, 
+die Installation läuft dann von alleine. 
+Der QR-code ist zufinden unter dem Ordner inst.
