4abd8b2e2624076c0556a5ea1dd714b6f137d95d
Can Kattwinkel <ck115@hdm-stuttgart.degit config --global user.email ck115@hdm-stuttgart.degit config --global user.email ck115@hdm-stuttgart.de>
2014-11-11 15:47:32 +0100

    gps01

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 // JavaScript Document
+
+
+
+
+window.onload = function() {
+	document.addEventListener("deviceready", function() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(success, error);
+		} else {
+			document.getElementById('karte').innerHTML = "Geolokation ist nicht möglich";
+		}
+	}, false);
+}
+function success(position) {
+	document.getElementById('karte').innerHTML = 
+	  "Das Objekt navigator.geolocation liefert:<hr />" + 
+	  "Breitengrad: " + position.coords.latitude + 
+	  "<br />L&auml;ngengrad: " + position.coords.longitude + 
+	  '<br />&Uuml;bergangsh&ouml;he: ' + position.coords.altitude + 
+	  '<br />' + 'Genauigkeit: ' + position.coords.accuracy + 
+	  '<br />' + 'Genauigkeit &Uuml;bergangsh&ouml;he: ' + position.coords.altitudeAccuracy + 
+	  '<br />' + 'Steuerkurs: ' + position.coords.heading + '<br />' + 
+	  'Geschwindigkeit: ' + position.coords.speed + '<br />' + 
+	  'Zeitstempel: ' + position.timestamp;
+}
+function error(msg) {
+	document.getElementById('karte').innerHTML = 
+	  "Fehler bei der Lokalisierung:<br /> " + msg;
+}
