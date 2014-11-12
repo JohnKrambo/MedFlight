cb4c9f9247c24339ac7d6a942d949d918be7ea5f
Can Kattwinkel <ck115@hdm-stuttgart.degit config --global user.email ck115@hdm-stuttgart.degit config --global user.email ck115@hdm-stuttgart.de>
2014-11-12 16:22:51 +0100

    maps und get location verbunden

--- js/app.js
+++ js/app.js
@@ -1,33 +1,77 @@
-// JavaScript Document
+ 
+ 
+ // GEO LOCATION GET LAT + LON
+ 
+ 
+ 		 geolocation =    	function(){
+		   		var options = {enableHighAccuracy: true};
+		   		watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+//function on Success gibt die Position zurück in einem PositionObjekt	
+	function onSuccess(position) {
+		var lat = position.coords.latitude;
+		var lng = position.coords.longitude;
+		
+		localStorage.setItem("latitude",lat);
+		localStorage.setItem("longitude",lng);
+		
+		//alert("lat: " + lat + "long: " + lng );
+	}	   		
+		function onError(error){
+			alert("you dont have service"+ error.message); 
+		}		   		
+		   		
+		   	}
+		   geolocation();
 
-
-
-/*
-
-window.onload = function() {
-	document.addEventListener("deviceready", function() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(success, error);
-		} else {
-			document.getElementById('karte').innerHTML = "Geolokation ist nicht möglich";
-		}
-	}, false);
-}
-function success(position) {
-	document.getElementById('karte').innerHTML = 
-	  "Das Objekt navigator.geolocation liefert:<hr />" + 
-	  "Breitengrad: " + position.coords.latitude + 
-	  "<br />L&auml;ngengrad: " + position.coords.longitude + 
-	  '<br />&Uuml;bergangsh&ouml;he: ' + position.coords.altitude + 
-	  '<br />' + 'Genauigkeit: ' + position.coords.accuracy + 
-	  '<br />' + 'Genauigkeit &Uuml;bergangsh&ouml;he: ' + position.coords.altitudeAccuracy + 
-	  '<br />' + 'Steuerkurs: ' + position.coords.heading + '<br />' + 
-	  'Geschwindigkeit: ' + position.coords.speed + '<br />' + 
-	  'Zeitstempel: ' + position.timestamp;
-}
-function error(msg) {
-	document.getElementById('karte').innerHTML = 
-	  "Fehler bei der Lokalisierung:<br /> " + msg;
-}
-
-*/
+ 
+ 
+ 
+ 	var map;
+   	var lat = localStorage.getItem("latitude") 
+   	var lng = localStorage.getItem("longitude") 
+	  var aktuellePosition =new google.maps.LatLng(lat,lng);
+	// aktivieren wenn wir irgendwann mit 2 Markern Arbeiten wollen ( 0: Standort, 1: Ankunft Drohen)
+	//  var ankunft =new google.maps.LatLng(48.17055,11.550579);
+      
+	  
+	  function initialize() {
+        var mapOptions = {
+          zoom: 12,
+          center: new google.maps.LatLng(lat,lng),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('map_canvas'),
+            mapOptions);			
+		
+		
+	
+		var marker =new google.maps.MarkerImage('img/marker.png');
+	
+		
+		
+		var marker1 = new google.maps.Marker({
+          position:aktuellePosition,
+          map: map,		
+		  icon:marker,
+          title: 'Aktuelle Position'
+        });
+		
+		
+		/*
+		
+		aktivieren wenn 2 Marker, s.o.
+		
+		var marker2 = new google.maps.Marker({
+          position:ankunft,
+		 
+          map: map,
+          title: 'Olympia Park'
+        });
+			*/	
+		
+	
+		
+	
+	  
+	  
+      }
