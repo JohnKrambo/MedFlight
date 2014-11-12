df77f2d42fa9aaeaa3acd097a3d66d2648873261
Can Kattwinkel <ck115@hdm-stuttgart.degit config --global user.email ck115@hdm-stuttgart.degit config --global user.email ck115@hdm-stuttgart.de>
2014-11-12 18:04:10 +0100

    fix versuch03

--- js/app.js
+++ js/app.js
@@ -1,46 +1,36 @@
  
  
- // GEO LOCATION GET LAT + LON
+ // GEO LOCATION GET LAT + LON 
  
- 
- 		 geolocation =    	function(){
-		   		var options = {enableHighAccuracy: true};
-		   		watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
-//function on Success gibt die Position zurück in einem PositionObjekt	
-	function onSuccess(position) {
-		var lat = position.coords.latitude;
-		var lng = position.coords.longitude;
-		
-		localStorage.setItem("latitude",lat);
-		localStorage.setItem("longitude",lng);
-		
-		//alert("lat: " + lat + "long: " + lng );
-	}	   		
-		function onError(error){
+ 		 geolocation = function(){
+		   	var options = {enableHighAccuracy: true};
+			watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+			//function on Success gibt die Position zurück in einem PositionObjekt	
+			function onSuccess(position) {
+				var lat = position.coords.latitude;
+				var lng = position.coords.longitude;	
+				//localStorage.setItem("latitude",lat);
+				//localStorage.setItem("longitude",lng);
+				alert("lat: " + lat + "long: " + lng );
+				 return [lat, lon]
+			}	   			
+			function onError(error){
 			alert("you dont have service"+ error.message); 
-		}		   		
-		   		
-		   	}
-		// 
+			}		   		   		
+		}
 
  
  
- 
-
-	  
-	  function initialize() {
-		  
-		  geolocation();
-		 	var map;
-   	var lat = localStorage.getItem("latitude") 
-   	var lng = localStorage.getItem("longitude") 
+	var temPos = geolocation();
+ 	var map;
+   	var lat = localStorage.getItem(temPos[0]) 
+   	var lng = localStorage.getItem(temPos[1]) 
 	  var aktuellePosition =new google.maps.LatLng(lat,lng);
 	// aktivieren wenn wir irgendwann mit 2 Markern Arbeiten wollen ( 0: Standort, 1: Ankunft Drohen)
 	//  var ankunft =new google.maps.LatLng(48.17055,11.550579);
-        
-		  
-		  
-		  
+      
+	  
+	  function initialize() {
         var mapOptions = {
           zoom: 12,
           center: new google.maps.LatLng(lat,lng),
