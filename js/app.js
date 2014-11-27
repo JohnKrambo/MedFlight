25a8567dc70a109caa47673ab4ab1d79c11b72f0
JohnKrambo <andreas.krappitz.1988@freenet.de>
2014-11-27 13:41:07 +0100

    added site for manual location entry and added countdown for drones arrival

--- js/app.js
+++ js/app.js
@@ -14,8 +14,9 @@
 				alert("lat: " + lat + "long: " + lng );
 				 return [lat, lon]
 			}	   			
+			//when geolocation doesn´t get a location
 			function onError(error){
-			alert("you dont have service"+ error.message); 
+				 localStorage.setItem("noService",true); 
 			}		   		   		
 		}
 
@@ -70,10 +71,37 @@
           title: 'Olympia Park'
         });
 			*/	
-		
-	
-		
-	
-	  
-	  
+			  
       }
+ function countdown(){
+var end = new Date('12/24/2014 10:1 AM');
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById('countdown').innerHTML = 'EXPIRED!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById('countdown').innerHTML = days + 'days ';
+        document.getElementById('countdown').innerHTML += hours + 'hrs ';
+        document.getElementById('countdown').innerHTML += minutes + 'mins ';
+        document.getElementById('countdown').innerHTML += seconds + 'secs';
+    }
+
+    timer = setInterval(showRemaining, 1000);
+}
